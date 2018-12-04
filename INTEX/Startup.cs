c122b918bd793e72bba6db6c7dd3e54789202bfd
Alec Ostheimer <alecostheimer94@gmail.com>
2018-12-04 16:40:53 -0700

    First INTEX commit

---
+++ INTEX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INTEX.Startup))]
+namespace INTEX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
