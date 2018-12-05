23a53c6e49b0a0141ebfd6feda95954779609646
benwalbrecht <benjaminwalbrecht@gmail.com>
2018-12-05 10:18:08 -0700

    Made severl models

---
+++ INTEX/Models/Orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Orders")]
+    public class Orders
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double QuotedPrice { get; set; }
+        public double ActualPrice { get; set; }
+
+        [Display(Name = "CustomerID")]
+        public  virtual int CustomerID { get; set; }
+        public  virtual Customer Customer { get; set; }
+
+        [Display(Name = "EmployeeID")]
+        public virtual int EmployeeID { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [Display(Name = "InvoiceID")]
+        public virtual int InvoiceID { get; set; }
+        public virtual Invoice Invoice { get; set; }
+
+        public virtual ICollection<WorkOrder> WorkOrders { get; set; }
+    }
+}
---
+++ INTEX/Models/PaymentInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("PaymentInfo")]
+    public class PaymentInfo
+    {
+        [Key]
+        public int PaymentInfoID { get; set; }
+        public String BillingAddress { get; set; }
+        public int CreditCardNo { get; set; }
+
+        [Display(Name = "CustomerID")]
+        public virtual int CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
