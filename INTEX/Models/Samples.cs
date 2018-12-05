cf3fbe2ca8efcc6c8be3cf86d22bb0b5b6f5fb22
benwalbrecht <benjaminwalbrecht@gmail.com>
2018-12-05 11:20:57 -0700

    Completed the addition of the models

--- INTEX/Models/Samples.cs
+++ INTEX/Models/Samples.cs
@@ -10,10 +10,20 @@ namespace INTEX.Models
     [Table("Samples")]
     public class Samples
     {
-        [Key]
+        [Key, Column(Order = 1)]
+        public int SequenceCode { get; set; }
+        [Key, Column(Order = 2)]
         public int LTNumber { get; set; }
-        public String CompoundName { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime ReceivedDate { get; set; }
+        public String Appearance { get; set; }
+        public float MolecularMass { get; set; }
+        public float MTD { get; set; }
+        public float ClientWeight { get; set; }
+        public float ActualWeight { get; set; }
 
-        public virtual ICollection<WorkOrder> WorkOrders { get; set; }
+        [Display(Name = "AssayID")]
+        public virtual int AssayID { get; set; }
+        public virtual Assay Assay { get; set; }
     }
 }
---
+++ INTEX/Models/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int TestID { get; set; }
+        public String Type { get; set; }
+        public String Description { get; set; }
+
+        public virtual ICollection<AssayTest> AssayTests { get; set; }
+        public virtual ICollection<TestMaterial> TestMaterials { get; set; }
+    }
+}
