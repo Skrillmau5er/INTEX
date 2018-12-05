fedf7c63f67f0897249422e6819d49c8dba687ff
Alec Ostheimer <alecostheimer94@gmail.com>
2018-12-04 20:09:44 -0700

    Updated database stuff

---
+++ INTEX/DAL/IntextContext.cs
@@ -0,0 +1,20 @@
+﻿using INTEX.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.DAL
+{
+    public class IntextContext : DbContext
+    {
+        public IntextContext() : base("IntextContext")
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
