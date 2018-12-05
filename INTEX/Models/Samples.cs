6d06bbbce7371ada46b71fc6f2e1d3495dca9e56
benwalbrecht <benjaminwalbrecht@gmail.com>
2018-12-05 10:52:27 -0700

    Minor stuff

--- INTEX/Models/Samples.cs
+++ INTEX/Models/Samples.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace INTEX.Models
 {
+    [Table("Samples")]
     public class Samples
     {
+        [Key]
+        public int LTNumber { get; set; }
+        public String CompoundName { get; set; }
+
+        public virtual ICollection<WorkOrder> WorkOrders { get; set; }
     }
 }
