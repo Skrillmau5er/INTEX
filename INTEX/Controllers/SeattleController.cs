e9ae93df6fda7e4440ae8c4bea10afa4db51b9ff
cash-allred <cash.allred@gmail.com>
2018-12-06 10:40:47 -0700

    getting ready to pull

--- INTEX/Controllers/SeattleController.cs
+++ INTEX/Controllers/SeattleController.cs
@@ -16,5 +16,34 @@ namespace INTEX.Controllers
         {
             return View();
         }
+
+        // GET: WorkOrders/Create
+        public ActionResult Create()
+        {
+            ViewBag.LTNumber = new SelectList(db.Compound, "LTNumber", "CompoundName");
+            ViewBag.OrderID = new SelectList(db.Order, "OrderID", "OrderID");
+            ViewBag.StatusID = new SelectList(db.Status, "StatusID", "StatusDescription");
+            return View();
+        }
+
+        // POST: WorkOrders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WorkOrderID,DueDate,SummaryReport,OrderID,StatusID,LTNumber")] WorkOrder workOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WorkOrder.Add(workOrder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LTNumber = new SelectList(db.Compound, "LTNumber", "CompoundName", workOrder.LTNumber);
+            ViewBag.OrderID = new SelectList(db.Order, "OrderID", "OrderID", workOrder.OrderID);
+            ViewBag.StatusID = new SelectList(db.Status, "StatusID", "StatusDescription", workOrder.StatusID);
+            return View(workOrder);
+        }
     }
 }
